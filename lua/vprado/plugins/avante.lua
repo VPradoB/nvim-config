63b9b1dd258d4485d5217e222c254cea0fce6fc0
Victor Prado <vmpradob@gmail.com>
2025-05-15 00:12:15 -0500

    test(avante): trying new avante config)

--- lua/vprado/plugins/avante.lua
+++ lua/vprado/plugins/avante.lua
@@ -8,7 +8,7 @@ return {
 			---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
 			provider = "copilot", -- Recommend using Claude
 			copilot = {
-				model = "claude-3.7-sonnet", -- o1-preview | o1-mini | claude-3.7-sonnet
+				model = "gpt-4.1", -- o1-preview | o1-mini | claude-3.7-sonnet
 			},
 			cursor_applying_provider = "copilot", -- In this example, use Groq for applying, but you can also use any provider you want.
 			auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
@@ -65,19 +65,19 @@ return {
 				wrap = true, -- similar to vim.o.wrap
 				width = 30, -- default % based on available width
 				sidebar_header = {
-					enabled = true, -- true, false to enable/disable the header
+					enabled = false, -- true, false to enable/disable the header
 					align = "center", -- left, center, right for title
-					rounded = false,
+					rounded = true,
 				},
 				input = {
-					prefix = "> ",
-					height = 8, -- Height of the input window in vertical layout
+					prefix = "",
+					height = 4, -- Height of the input window in vertical layout
 				},
 				edit = {
 					start_insert = true, -- Start insert mode when opening the edit window
 				},
 				ask = {
-					floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+					floating = true, -- Open the 'AvanteAsk' prompt in a floating window
 					start_insert = true, -- Start insert mode when opening the ask window
 					---@type "ours" | "theirs"
 					focus_on_apply = "ours", -- which diff to focus after applying
@@ -125,7 +125,7 @@ return {
 							insert_mode = true,
 						},
 						-- required for Windows users
-						use_absolute_path = true,
+						use_absolute_path = false,
 					},
 				},
 			},
