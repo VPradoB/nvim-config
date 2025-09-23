01c1b2664ef1d25864c28d036dcddc7e65b1eaf5
Victor Prado <vmpradob@gmail.com>
2025-09-23 12:47:11 -0300

    feat: remove ruff and re add lua_lsp

---
+++ lua/vprado/plugins/lazy_dev.lua
@@ -0,0 +1,13 @@
+return {
+	-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+	-- used for completion, annotations and signatures of Neovim apis
+	-- necesary to recognize vim.**
+	"folke/lazydev.nvim",
+	ft = "lua",
+	opts = {
+		library = {
+			-- Load luvit types when the `vim.uv` word is found
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+		},
+	},
+}
--- lua/vprado/plugins/nvim_lsp.lua
+++ lua/vprado/plugins/nvim_lsp.lua
@@ -208,7 +208,6 @@ return {
 			-- clangd = {},
 			-- gopls = {},
 			pyright = {},
-			ruff = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
@@ -235,22 +234,10 @@ return {
 			},
 		}
 
-		-- Ensure the servers and tools above are installed
-		--
-		-- To check the current status of installed tools and/or manually install
-		-- other tools, you can run
-		--    :Mason
-		--
-		-- You can press `g?` for help in this menu.
-		--
-		-- `mason` had to be setup earlier: to configure its options see the
-		-- `dependencies` table for `nvim-lspconfig` above.
-		--
-		-- You can add other tools here that you want Mason to install
-		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
+			"pyright",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
