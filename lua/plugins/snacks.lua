7c8e04ce41af9578f8f23922f47cafd8a5429cd0
victor prado <vmpradob@gmail.com>
2025-11-19 22:45:05 -0300

    feat: add scratch files and some snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,13 +2,6 @@ return {
   "folke/snacks.nvim",
   opts = {
     explorer = { enabled = false },
-    picker = {
-      sources = {
-        explorer = {
-          -- your explorer picker configuration comes here
-          -- or leave it empty to use the default settings
-        },
-      },
-    },
+    picker = { enabled = true },
   },
 }
